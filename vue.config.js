8e1b8fc5f86707a962fa892a346d04c0460fc3a0
Olang Daniel <olangdan17@gmail.com>
2022-04-30 17:43:38 +0100

    ghpages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: 'https://asterisk-z.github.io/dictionary-app/public/'
+  publicPath: '/dictionary-app/public/'
 })
